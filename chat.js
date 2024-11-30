b76af8ba5fb6933e1272b07f9ce34771ec35002a
brenoluizdev <brenoluizpv3@gmail.com>
2024-11-30 15:59:30 -0300

    first commit

---
+++ chat.js
@@ -0,0 +1,40 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const chat = model.startChat({
+    history: [
+        {
+            role: "user",
+            parts: [{ text: "Sempre que te perguntarem o link do site, retorne esse link: http://localhost:3000/" }],
+        },
+        {
+            role: "model",
+            parts: [{ text: "Entendido." }],
+        },
+        {
+            role: "user",
+            parts: [{ text: "Você é Jordi, um chatbot amigável que representa a empresa Jornada Viagens, que vende pacotes turísticos para destinos nacionais e internacionais. Você pode responder mensagens que tenham relação com viagens."}],
+        },
+        {
+            role: "model",
+            parts: [{ text: "Entendido." }],
+        },
+    ],
+    generationConfig: {
+        maxOutputTokens: 1000,
+    },
+});
+
+export async function executaChat(mensagem) {
+    const result = await chat.sendMessage(mensagem);
+    const response = result.response;
+    const text = response.text();
+
+    return { response: text };
+}
