6cccaa69ff0a2baefef43f6781f84761219a0df5
brenoluizdev <brenoluizpv3@gmail.com>
2024-11-30 16:10:18 -0300

    ajustes

--- chat.js
+++ chat.js
@@ -1,6 +1,7 @@
 import { chat } from "./inicializaChat.js";
 
 export async function executaChat(mensagem) {
+    console.log("Tamanho do histórico: " + (await chat.getHistory()).length);
     const result = await chat.sendMessage(mensagem);
     const response = result.response;
     const text = response.text();
